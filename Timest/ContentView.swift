45b3d2911f03912241e57f60d420cbb51177e93c
dekobokoengineer <dekobokoenginner@outlook.jp>
2024-07-17 17:33:52 +0900

    Initial Commit

---
+++ Timest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Timest
+//
+//  Created by ryo on 2024/07/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Timest/TimestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimestApp.swift
+//  Timest
+//
+//  Created by ryo on 2024/07/17.
+//
+
+import SwiftUI
+
+@main
+struct TimestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
